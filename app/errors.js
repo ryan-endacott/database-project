4f828dd3f4b477ff7f42e196b77ab8f8623bbcc1
Ryan Endacott <rzeg24@gmail.com>
2013-10-28 20:47:52 -0500

    added unauthorized error

--- app/errors.js
+++ app/errors.js
@@ -2,6 +2,16 @@ module.exports = {
 
   badRequestError: function(err, res) {
     res.send(400, err);
+  },
+
+  unauthorizedError: function(err, res) {
+    res.send(401, {
+      error: {
+        code: 401,
+        type: 'Unauthorized'
+      },
+      message: 'Failed to authenticate with API token.'
+    });
   }
 
 };
