220d556143ea85cccde90cd29b911597fe689d11
Ryan Endacott <rzeg24@gmail.com>
2013-10-28 20:08:50 -0500

    basics of user model implemented

--- app/db.js
+++ app/db.js
@@ -15,7 +15,74 @@ db.once('open', function() {
 });
 
 var userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  token: String,
+  name: String
 });
+
+
+// Generate user API token
+userSchema.pre('save', function(next) {
+  // Maybe make this pre init
+  // If no user API token already added
+  if (!this.token || this.token.length != 36) {
+    this.token = uuid.v4();
+  }
+
+  next();
+});
+
+// Bcrypt middleware for hashing password
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if(!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if(err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+// Password verification
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+
+// Don't return token of any user
+// specify the transform schema option
+if (!userSchema.options.toJSON) userSchema.options.toJSON = {};
+userSchema.options.toJSON.transform = function (doc, ret, options) {
+  delete ret._id;
+  delete ret.token;
+  delete ret.__v;
+  delete ret.password;
+  delete ret.contacts;
+}
+
+// Don't send password hash on register.
+if (!userSchema.options.toObject) userSchema.options.toObject = {};
+userSchema.options.toObject.transform = function (doc, ret, options) {
+  delete ret.password;
+}
+
 
 module.exports = {
   User: mongoose.model('User', userSchema)
