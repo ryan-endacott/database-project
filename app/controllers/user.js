26700ad8922bfd1c96160c73731d0a01cf964cde
Ryan Endacott <rzeg24@gmail.com>
2013-10-28 20:34:53 -0500

    added user register action

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,18 @@
 var db = require('../db'),
-    User = db.User;
+    User = db.User,
+    errors = require('../errors'),
+    badRequest = errors.badRequestError;
+
+// Action to register a new user.
+exports.register = function(req, res) {
+
+  var user = new User(req.body.user);
+  user.save(function(err, user) {
+    if (err) return badRequest(err, res);
+    res.json(user.toObject()); // Send toObject to include user's token
+  });
+
+};
+
 
 
--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,10 @@
 // Set up routes for the app
 
 // Controllers
-var index = require('../app/controllers/index');
+var index = require('../app/controllers/index'),
+    user = require('../app/controllers/user');
 
 module.exports = function(app) {
   app.get('/', index.index);
-}
+  app.post('/user/register', user.register);
+};
